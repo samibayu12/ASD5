d932360ac681c8bdfb197c17379ce2f6a987d3d6
samibayu <boysami52gmail.com>
2022-03-03 16:38:51 +0300

    new2

--- calculator_test.py
+++ calculator_test.py
@@ -7,7 +7,21 @@ class Test(unittest.TestCase):
     calculator = CalculatorClass.Calculator() # instantiate the Calculator Class
     def test_0_add(self):
         result = self.calculator.add(4,8)
-        self.assertEqual(result,12)
+        self.assertEquals(result,12)
+
+
+    def test_0_subtarct(self):
+        result = self.calculator.subtarct(4,3)
+        self.assertEquals(result,1)
+
+    def test_0_multiply(self):
+        result = self.calculator.multiply(4,2)
+        self.assertEquals(result, 8)
+
+    def test_0_divide(self):
+        result = self.calculator.divide(4,2)
+        self.assertEquals(result, 2)
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()
+    
